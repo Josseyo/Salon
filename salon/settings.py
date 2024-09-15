750251612dfa3eaa154b1247e6ee962ba59299f4
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-09-15 13:37:50 +0000

    Add mobile_header and main navbar

--- salon/settings.py
+++ salon/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-#from pathlib import Path
+# from pathlib import Path
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR,...)

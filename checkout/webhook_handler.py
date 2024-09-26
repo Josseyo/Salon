be3be271d3074432eb9a323737948bb91f010629
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-09-26 13:45:28 +0000

    complete webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -12,5 +12,32 @@ class StripeWH_Handler:
         Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+
+        intent = event.data.object
+        pid = intent.id
+        bag = intent.metadata.bag
+        save_info = intent.metadata.save_info
+
+        # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
+
+        billing_details = stripe_charge.billing_details # updated
+        shipping_details = intent.shipping
+        grand_total = round(stripe_charge.amount / 100, 2) # updated
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
+        """
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
--- env.py
+++ env.py
@@ -1,5 +1,10 @@
 import os
 
 # Set Stripe keys as environment variables
-os.environ['STRIPE_PUBLIC_KEY'] = 'pk_test_51Q28sSP7j0wPuJKQmqSgJ4kgqTyWrlsQi03lZBbhRMAKZcvaTmoCbLDk4cjvI0bS5p8IUxLVlt0BJ7IWqMMOjViP00DbARntlQ'
-os.environ['STRIPE_SECRET_KEY'] = 'sk_test_51Q28sSP7j0wPuJKQd9By9PTgKGSUFRRk9Q2dUiiyQdJKG4u8oa80jP2cXQj0t14VPVTelAFVzzTmeEYXcjVxL8Kv00TuzQdu4j'
+os.environ["STRIPE_PUBLIC_KEY"] = (
+    "pk_test_51Q28sSP7j0wPuJKQmqSgJ4kgqTyWrlsQi03lZBbhRMAKZcvaTmoCbLDk4cjvI0bS5p8IUxLVlt0BJ7IWqMMOjViP00DbARntlQ"
+)
+os.environ["STRIPE_SECRET_KEY"] = (
+    "sk_test_51Q28sSP7j0wPuJKQd9By9PTgKGSUFRRk9Q2dUiiyQdJKG4u8oa80jP2cXQj0t14VPVTelAFVzzTmeEYXcjVxL8Kv00TuzQdu4j"
+)
+os.environ["STRIPE_WH_SECRET"] = "whsec_cdZiCH4tBRukW5PSNJ3Q3ZFnKC4gPyiE"

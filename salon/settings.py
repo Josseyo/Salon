a4d7940031ab516bf7d8176c03317e1e61721bdf
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-10-04 07:59:10 +0000

    update STATICFILES_DIRS

--- salon/settings.py
+++ salon/settings.py
@@ -183,8 +183,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 

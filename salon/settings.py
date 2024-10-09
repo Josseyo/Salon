ccbd3dff794ab14c65baf4185147f4b9a2f3dfa6
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-10-09 09:58:58 +0000

    change default email

--- salon/settings.py
+++ salon/settings.py
@@ -257,7 +257,7 @@ DEFAULT_FROM_EMAIL = ("salontalks@example.com",)
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+    DEFAULT_FROM_EMAIL = 'salon@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True

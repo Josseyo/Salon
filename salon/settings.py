22e86281c9e0fa7f24bc13f63d6805290a93b2ca
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-10-16 10:42:39 +0000

    set DEBUG=False

--- salon/settings.py
+++ salon/settings.py
@@ -43,7 +43,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = (
     os.environ.get("ALLOWED_HOSTS", "").split(",")

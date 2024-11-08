689eceff9de948276a247073827be8a43646dfac
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-11-08 12:19:24 +0000

    BUG FIX #42 forts. Include responded field in form and Meta class for proper validation.

--- common/forms.py
+++ common/forms.py
@@ -3,10 +3,31 @@ from django import forms
 from contact.models import Contact
 from django.core.validators import EmailValidator
 
-
 class ContactForm(forms.ModelForm):
+    responded = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput())
+
     class Meta:
         model = Contact
-        fields = ("name", "email", "subject", "message")
+        fields = ("name", "email", "subject", "message", "responded")
 
-    email = forms.EmailField(validators=[EmailValidator()])
+    email = forms.EmailField(validators=[EmailValidator()], error_messages={
+        'invalid': 'Please enter a valid email address.'
+    })
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if not name:
+            raise forms.ValidationError("This field is required.")
+        return name
+
+    def clean_subject(self):
+        subject = self.cleaned_data.get('subject')
+        if not subject:
+            raise forms.ValidationError("This field is required.")
+        return subject
+
+    def clean_message(self):
+        message = self.cleaned_data.get('message')
+        if not message:
+            raise forms.ValidationError("This field is required.")
+        return message

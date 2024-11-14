1659a7ac1493cbcea45a014d0de074ac4208e387
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-11-14 16:29:55 +0000

    remove comments

--- contact/models.py
+++ contact/models.py
@@ -16,16 +16,16 @@ class Contact(models.Model):
 
     name = models.CharField(
         max_length=254, default=""
-    )  # Added default value
+    )
     email = models.EmailField(
         default="default@example.com"
-    )  # Added default value
+    )
     subject = models.CharField(
         choices=SUBJECTS, max_length=254, default="Other"
-    )  # Added default value
+    )
     message = models.TextField(
         max_length=1024, default=""
-    )  # Added default value
+    )
     date_created = models.DateTimeField(default=timezone.now)
     responded = models.BooleanField(default=False)
 
